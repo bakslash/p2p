c61c718f97e0c5153069ccd05ddd0674285b78b6
marcos muasa <marcusmuasa@gmail.com>
2023-12-08 22:23:49 +0300

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-//import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
   plugins: [
-    laravel([
-      'resources/js/app.jsx',
-    ]),
-  //  reactRefresh(), // Remove the array brackets around reactRefresh
+    laravel({
+      manifest: true,
+      entry: 'resources/js/app.jsx',
+    }),
   ],
   
   build: {
