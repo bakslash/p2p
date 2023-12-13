01731c2c4b19bb335f4ce43da39753a59de5d253
bakslash <marcusmuasa@gmail.com>
2023-12-13 09:44:41 +0300

    errsfix

--- vite.config.js
+++ vite.config.js
@@ -10,19 +10,9 @@ export default defineConfig({
     }),
   ],
 
-  server: {
-    proxy: {
-      '/': {
-        target: 'http://143.198.157.55', // Removed trailing slash
-        changeOrigin: true,
-      },
-    },
-  },
+  
 
-  optimizeDeps: {
-    include: ['react-dom', 'react-dom/server'],
-  },
-
+ 
   build: {
     outDir: 'public/build',
     manifest: true,
